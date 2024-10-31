808e93155240fcab798268267f2debdd93207a97
n1tebb <kolasnikovegor@gmail.com>
2024-10-31 15:33:40 +0500

    asddddada

--- 2910/main.cpp
+++ 2910/main.cpp
@@ -24,6 +24,15 @@ void Start();
 bool Login();
 void ShopUserMenu();
 void ShopAdminMenu();
+void ShopUserMenu();
+void CreateStaticStorage();
+void ShowStorage();
+
+template<typename ArrType>
+void SetArr(ArrType staticArr, ArrType dynamicArr, int size);
+
+template<typename ArrType>
+void PrintArr(ArrType dynamicArr, int size);
 
 #if defined(max)
 #undef max
@@ -35,23 +44,48 @@ int main()
 	Start();
 	delete[]loginArr;
 	delete[]passwordArr;
+	delete[]idArr;
+	delete[]countArr;
+	delete[]priceArr;
+	delete[]nameArr;
 	return 0;
 }
+
 
 void Start()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	std::cout << "\n\n\t\tAVI\n\n\n";
+
+
+
 	if (Login() == true)
 	{
 		//std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		if (isAdmin == true)
 		{
+			std::string choose;
+			do
+			{
+				std::cout << "Использовать готовый склад или создать новый?\n";
+				std::cout << "1 - готовый склад\n2 - Создать новый\n\nВвод: ";
+				std::getline(std::cin, choose, '\n');
+			} while (choose.size() > 1 || choose[0] < 49 || choose[0] > 50);
+
+			if (choose == "1")
+			{
+				CreateStaticStorage();
+			}
+			else
+			{
+
+			}
 			ShopAdminMenu();
 		}
 		else
 		{
+			CreateStaticStorage();
 			ShopUserMenu();
 		}
 	}
@@ -122,7 +156,7 @@ void ShopAdminMenu()
 		}
 		else if (choose == "2")
 		{
-
+			ShowStorage();
 		}
 		else if (choose == "3")
 		{
@@ -157,4 +191,58 @@ void ShopAdminMenu()
 			std::cerr << "MainMenuErorr";
 		}
 	}
+}
+
+void CreateStaticStorage()
+{
+	const int staticSize = 10;
+	int idStaticArr[staticSize]{ 1,2,3,4,5,6,7,8,9,10 };
+
+	std::string nameStaticArr[staticSize]
+	{ "nike air force 1 white               ", "raf simons ozweego 3               ",
+	  "balenciaga 3xl                    ", "nike uptempo                     ",
+	  "nike x Travis Scott Air jordan 1",
+	  "balenciaga 10 xl                      ", "adidas campus                   ",
+	  "adidas samba                           ", "rick owens DRKSHDW               ","new rock                            "
+	};
+
+	int countStaticArr[staticSize]{ 35,3,4,8,5,2,10,10,5,5 };
+
+	double priceStaticArr[staticSize]{ 15000.0, 140000.0, 96000.0, 20000.0, 51000.0, 120000.0, 2300.0, 11000.0, 88000.0, 52000.0 };
+
+
+	SetArr(idStaticArr, idArr, staticSize);
+	SetArr(nameStaticArr, nameArr, staticSize);
+	SetArr(countStaticArr, countArr, staticSize);
+	SetArr(priceStaticArr, priceArr, staticSize);
+}
+
+void ShowStorage()
+{
+	std::cout << "ID\tНазвание\t\t\t\tЦена\tКоличество\n\n";
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << idArr[i] << "\t" << nameArr[i] << "\t" << priceArr[i] << "\t" << countArr[i] << "\n";
+
+	}
+
+}
+
+template<typename ArrType>
+void SetArr(ArrType staticArr, ArrType dynamicArr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		dynamicArr[i] = staticArr[i];
+	}
+}
+
+template<typename ArrType>
+void PrintArr(ArrType dynamicArr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << dynamicArr[i] << " ";
+	}
+	std::cout << "\n";
 }
